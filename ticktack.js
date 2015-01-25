ae86d452373362dce0e3d06619a636ba6a1beb8b
David Aerne <meodai@gmail.com>
2015-01-25 13:10:20 +0100

    inital commit

---
+++ ticktack.js
@@ -0,0 +1,55 @@
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function(f){setTimeout(f, 1000/60);};
+
+var watch = (function (undefined) {
+  'use strict';
+  var callbacks, registeredCallbacks, tick, getTimeObject;
+
+  callbacks = {};
+  registeredCallbacks = [];
+
+  getTimeObject = function () {
+    var now = new Date();
+
+    return {
+    //   ms: {
+    //     value:
+    //   }
+    };
+
+  };
+
+  tick = function () {
+
+    window.requestAnimationFrame(tick);
+    if (registeredCallbacks.length === 0) {
+      return;
+    }
+
+
+    // get new data structure
+
+
+    // iterater over registeredCallbacks
+      // if tick -> call -> return
+
+      // check difference for xxx to before and call if needed.
+
+    // set new data structure to previous
+  };
+
+  window.requestAnimationFrame(tick);
+
+  return {
+    on: function (eventName, callback) {
+
+      if (callbacks[eventName] === undefined) {
+        registeredCallbacks.push(eventName);
+        callbacks[eventName] = [];
+      }
+
+      callbacks[eventName].push();
+      // TODO pass data into the CB
+      callback();
+    }
+  };
+})();
