09857c5126c41f1e5583a6a58af818c34c4e9cb3
David Aerne <meodai@gmail.com>
2015-01-26 11:16:23 +0100

    adds optional AMD
    and some code linting

--- ticktack.js
+++ ticktack.js
@@ -1,124 +1,163 @@
-window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function(f){setTimeout(f, 1000/60);};
+'use strict';
 
-var watch = (function (undefined) {
-  'use strict';
-  var callbacks, registeredCallbacks, tick, getTimeObject, runCallbacks, timeObject;
+(function (root, factory) {
+  // optional AMD https://github.com/umdjs/umd/blob/master/amdWeb.js
+  if (typeof define === 'function' && define.amd) {
+      // AMD. Register as an anonymous module.
+      define(['requestAnimationFrame'], factory);
+  } else {
+      // Browser globals
+      root.ticktack = factory(root.$);
+  }
+}(this, function () {
 
-  callbacks = {};
-  registeredCallbacks = [];
+  function ticktack(undefined) {
 
-  runCallbacks = function(obj) {
-    var i, j, name;
-    for (i in registeredCallbacks) {
-      name = registeredCallbacks[i];
-      if( (obj[name] && obj[name].hasChanged ) || name === 'tick') {
-        for(j in callbacks[name]) {
-          callbacks[name][j].call(obj[name], obj, obj[name]);
-        }
-      }
-    }
-  };
+    var callbacks, registeredCallbacks, tick, getTimeObject, runCallbacks, timeObject;
 
-  getTimeObject = function (previousDigits) {
-    var now, timeInMilliseconds, digit, digits, d;
+    // Initializing callback objects
+    callbacks = {};
+    registeredCallbacks = [];
 
-    now = new Date();
-    timeInMilliseconds = now.getTime() - now.getTimezoneOffset() * 60000;
-
-    digits = {
-      'year': {
-        method: 'getFullYear',
-        value: 0,
-        progress: 0
-      },
-      'month': {
-        method: 'getMonth',
-        value: 0,
-        progress: 0,
-        getProgress: function(value) {
-          return value / 30;
+    /**
+     * runCallbacks : loops callbacks and calls them
+     * @param   {Object} obj object produced by getTimeObject, will be passed to the callback
+     * @returns {void}
+     */
+    runCallbacks = function(obj) {
+      var i, j, name;
+      for (i in registeredCallbacks) {
+        name = registeredCallbacks[i];
+        if ((obj[name] && obj[name].hasChanged ) || name === 'tick') {
+          for (j in callbacks[name]) {
+            callbacks[name][j].call(obj[name], obj, obj[name]);
+          }
         }
-      },
-      'day': {
-        method: 'getDay',
-        value: 0,
-        progress: 0,
-        getProgress: function(value) {
-          return now.getHours()/24;
-        }
-      },
-      'hour': {
-        method: 'getHours',
-        value: 0,
-        progress: 0,
-        getProgress: function(value) {
-          return (timeInMilliseconds / 1000 / 60 / 60) % 24 - value;
-        }
-      },
-      'minute': {
-        method: 'getMinutes',
-        value: 0,
-        progress: 0,
-        getProgress: function(value) {
-          return (timeInMilliseconds / 1000 / 60) % 60 - value;
-        }
-      },
-      'second': {
-        method: 'getSeconds',
-        value: 0,
-        progress: 0,
-        getProgress: function(value) {
-          return (timeInMilliseconds / 1000) % 60 - value;
-        }
-      },
-      'millisecond': {
-        method: 'getMilliseconds',
-        value: 0
       }
     };
 
-    for (digit in digits) {
-      d = digits[digit];
-      d.value = now[d.method]();
-      if (d.getProgress) {
-        d.progress = d.getProgress(d.value);
-      }
-      if ( previousDigits && previousDigits[digit] ) {
-        d.hasChanged = d.value !== previousDigits[digit].value;
-      }
-    }
+    /**
+     * getTimeObject : produces an object containing all values and relative values for every digit in Date()
+     * @param   {Object} previousDigits object previously obtained by getTimeObject to know what values did change
+     * @returns {Object}                value and progress for every digit in Date()
+     */
+    getTimeObject = function (previousDigits) {
+      var now, timeInMilliseconds, digit, digits, d;
 
-    return digits;
-  };
+      now = new Date();
+      timeInMilliseconds = now.getTime() - now.getTimezoneOffset() * 60000;
 
-  tick = function () {
+      digits = {
+        'year': {
+          method: 'getFullYear',
+          value: 0,
+          progress: 0
+        },
+        'month': {
+          method: 'getMonth',
+          value: 0,
+          progress: 0,
+          getProgress: function(value) {
+            return value / 30;
+          }
+        },
+        'day': {
+          method: 'getDay',
+          value: 0,
+          progress: 0,
+          getProgress: function() {
+            return now.getHours() / 24;
+          }
+        },
+        'hour': {
+          method: 'getHours',
+          value: 0,
+          progress: 0,
+          getProgress: function(value) {
+            return (timeInMilliseconds / 1000 / 60 / 60) % 24 - value;
+          }
+        },
+        'minute': {
+          method: 'getMinutes',
+          value: 0,
+          progress: 0,
+          getProgress: function(value) {
+            return (timeInMilliseconds / 1000 / 60) % 60 - value;
+          }
+        },
+        'second': {
+          method: 'getSeconds',
+          value: 0,
+          progress: 0,
+          getProgress: function(value) {
+            return (timeInMilliseconds / 1000) % 60 - value;
+          }
+        },
+        'millisecond': {
+          method: 'getMilliseconds',
+          value: 0
+        }
+      };
+
+      for (digit in digits) {
+        d = digits[digit];
+        // sets the value using the appropriate Date method
+        d.value = now[d.method]();
+        if (d.getProgress) {
+          // calculates the relative progress
+          d.progress = d.getProgress(d.value);
+        }
+        // compares with previous digits object to track changes
+        if (previousDigits && previousDigits[digit]) {
+          d.hasChanged = d.value !== previousDigits[digit].value;
+        }
+      }
+
+      return digits;
+    };
+
+    /**
+     * tick : loop used for RAF also applies the callbacks
+     * @returns {void}
+     */
+    tick = function () {
+      window.requestAnimationFrame(tick);
+
+      if (registeredCallbacks.length === 0) {
+        return;
+      }
+
+      // get new data structure
+      timeObject = getTimeObject(timeObject);
+      // runs callbacks
+      runCallbacks(timeObject);
+    };
 
     window.requestAnimationFrame(tick);
 
-    if (registeredCallbacks.length === 0) {
-      return;
-    }
+    // public methods
+    return {
+      /**
+       * on : registers a callback on a names-pace
+       * @param   {String}   eventName the name matching the digits returned by getTimeObject
+       * @param   {Function} callback  callback function
+       * @returns {void}
+       */
+      on: function (eventName, callback) {
+        if (callbacks[eventName] === undefined) {
+          registeredCallbacks.push(eventName);
+          callbacks[eventName] = [];
+        }
 
-    // get new data structure
-    timeObject = getTimeObject(timeObject);
+        callbacks[eventName].push(callback);
 
-    runCallbacks(timeObject);
-  };
+        tick();
 
-  window.requestAnimationFrame(tick);
-
-  return {
-    on: function (eventName, callback) {
-      if (callbacks[eventName] === undefined) {
-        registeredCallbacks.push(eventName);
-        callbacks[eventName] = [];
+        callback.call(timeObject[eventName], timeObject, timeObject[eventName]);
       }
+    };
 
-      callbacks[eventName].push(callback);
+  }
 
-      tick();
-
-      callback.call(timeObject[eventName], timeObject, timeObject[eventName]);
-    }
-  };
-})();
+  return (ticktack)();
+}));
