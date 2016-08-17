cc7c18b374851fde2e45e0fe22129ab3926486b4
David Aerne <meodai@gmail.com>
2016-08-17 20:55:52 +0200

    use a clock as header

--- main.js
+++ main.js
@@ -10,17 +10,13 @@ function rotate(el, rotation) {
 
 hljs.initHighlightingOnLoad();
 
-let direction = 1;
 ticktack.on('tick', digits => {
-  //rotate(sec, digits.getMinute().progress * 360);
-  let progress = digits.getMinute().progress;
-
-  if( (progress < 0.1 && direction < 0) ) {
-    direction *= -1;
-    titleProgress.style.transformOrigin = `${(direction < 0 ? 0 : 1) * 100}% 0`;
-  }
-
+  rotate(sec, digits.getMinute().progress * 360);
   d.documentElement.style.setProperty('--c-light', `hsl(${digits.getHour().progress * 360},100%,90%)`);
-
-  titleProgress.style.transform = `scaleX(${progress})`;
+});
+ticktack.on('minute', digits => {
+  rotate(min, digits.getHour().progress * 360);
+});
+ticktack.on('hour', digits => {
+  rotate(hour, digits.getDay().progress * 360);
 });
