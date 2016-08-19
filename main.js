d86810e4905a49d8d23fa5aa47918d9dc417a307
David Aerne <meodai@gmail.com>
2016-08-19 20:32:08 +0200

    makes JS more shitty browsers friendly

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-const d     = document;
-const sec   = d.querySelector('.js-sec');
-const min   = d.querySelector('.js-min');
-const hour  = d.querySelector('.js-hour');
+var d     = document;
+var sec   = d.querySelector('.js-sec');
+var min   = d.querySelector('.js-min');
+var hour  = d.querySelector('.js-hour');
 
 function rotate(el, rotation) {
   //el.setAttribute('transform', 'rotate('+ rotation +' 500 500)');
-  el.style.transform = `rotate(${rotation}deg)`;
+  el.style.transform = 'rotate(' + rotation +'deg)';
 }
 
 hljs.initHighlightingOnLoad();
 
-ticktack.on('tick', digits => {
+ticktack.on('tick', function(digits) {
   rotate(sec, digits.getMinute().progress * 360);
-  d.documentElement.style.setProperty('--c-light', `hsl(${digits.getHour().progress * 360},100%,90%)`);
+  d.documentElement.style.setProperty('--c-light', 'hsl(' + digits.getHour().progress * 360 + ',100%,90%)');
 });
-ticktack.on('minute', digits => {
+ticktack.on('minute', function(digits) {
   rotate(min, digits.getHour().progress * 360);
 })
-ticktack.on('hour', digits => {
+ticktack.on('hour', function(digits) {
   rotate(hour, digits.getDay().progress * 720);
 });
 
-setTimeout(() => {
+setTimeout(function() {
   d.body.classList.add('show');
 }, 2000);
